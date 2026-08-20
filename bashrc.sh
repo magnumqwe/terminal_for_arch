d5c0a0a2f1aa462d87ee3df641340669983d3329
Magnum <terk1nterraria10@gmail.com>
2026-08-20 19:27:13 +0300

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -1,18 +1,70 @@
-# проверка интерактивности (чтобы не ломать scp/rsync)
-case $- in
-    *i*) ;;
-      *) return;;
-esac
+export EDITOR="nvim"
+export VISUAL="nvim"
+export LESSHISTFILE=-
 
-# Подгрузка dircolors только если файл реально существует
-[ -f ~/.dir_colors ] && eval "$(dircolors ~/.dir_colors)"
+alias vim=nvim
+alias ls='ls --color=auto'
+alias ll='ls -alh --color=auto'
+alias grep='grep --color=auto'
+alias neofetch='fastfetch'
+alias fetch='fastfetch'
+alias lss='du -ah --max-depth 1'
 
-# Безопасная проверка наличия ag для fzf
-if command -v ag &> /dev/null; then
-    export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-fi
+PS1="\[\e[1;33m\]\$(parse_git_branch)\[\033[34m\]\$(parse_git_dirty)\n\[\033[1;34m\] 󰣇 \[\e[1;37m\] \w \[\e[1;36m\]\[\e[0;37m\] "
 
-# Вызов fastfetch если он установлен
+ex ()
+{
+    if [ -f $1 ] ; then
+      case $1 in
+        *.tar.bz2)   tar xjf $1   ;;
+        *.tar.gz)    tar xzf $1   ;;
+        *.bz2)       bunzip2 $1   ;;
+        *.rar)       unrar x $1   ;;
+        *.gz)        gunzip $1    ;;
+        *.tar)       tar xf $1    ;;
+        *.tbz2)      tar xjf $1   ;;
+        *.tgz)       tar xzf $1   ;;
+        *.zip)       unzip $1     ;;
+        *.Z)         uncompress $1;;
+        *.7z)        7za e x $1   ;;
+        *.deb)       ar x $1      ;;
+        *.tar.xz)    tar xf $1    ;;
+        *.tar.zst)   unzstd $1    ;;
+        *)           echo "'$1' cannot be extracted via ex()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
+
+HISTSIZE=10000
+
+export LESS_TERMCAP_mb=$'\e[1;36m'
+export LESS_TERMCAP_md=$'\e[1;36m'
+export LESS_TERMCAP_me=$'\e[1;37m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;34m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;34m'
+
+
+parse_git_branch() {
+  git rev-parse --abbrev-ref HEAD 2> /dev/null
+}
+
+
+function parse_git_dirty {
+  STATUS="$(git status 2> /dev/null)"
+  if [[ $? -ne 0 ]]; then printf ""; return; else printf " ["; fi
+  if echo "${STATUS}" | grep -c "renamed:"         &> /dev/null; then printf " >"; else printf ""; fi
+  if echo "${STATUS}" | grep -c "branch is ahead:" &> /dev/null; then printf " !"; else printf ""; fi
+  if echo "${STATUS}" | grep -c "new file::"       &> /dev/null; then printf " +"; else printf ""; fi
+  if echo "${STATUS}" | grep -c "Untracked files:" &> /dev/null; then printf " ?"; else printf ""; fi
+  if echo "${STATUS}" | grep -c "modified:"        &> /dev/null; then printf " *"; else printf ""; fi
+  if echo "${STATUS}" | grep -c "deleted:"         &> /dev/null; then printf " -"; else printf ""; fi
+  printf " ]"
+}
+
 if command -v fastfetch &> /dev/null; then
     fastfetch
 fi
